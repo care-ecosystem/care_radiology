a43aca093025b4cd3b8f973d6dce47d8fc67ba89
Jagankumar E <53823168+jagankumar-egov@users.noreply.github.com>
2026-09-18 18:18:35 +0530

    ENG-1028 :: Fix to change approach in validation of required values and validate at runtime (#159)
    https://openhealthcarenetwork.atlassian.net/browse/CPLUG-112

--- src/care_radiology/apps.py
+++ src/care_radiology/apps.py
@@ -1,7 +1,19 @@
+import sys
+
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 PLUGIN_NAME = "care_radiology"
+
+# Management commands that run during build time - skip validation for these
+BUILD_TIME_COMMANDS = {
+    "collectstatic",
+    "makemigrations",
+    "migrate",
+    "compilemessages",
+    "check",
+    "spectacular",
+}
 
 
 class CareRadiologyPluginConfig(AppConfig):
@@ -17,3 +29,12 @@ class CareRadiologyPluginConfig(AppConfig):
         PermissionController.register_permission_handler(RadiologyPermissions)
 
         import care_radiology.security.access  # noqa: F401
+
+        # Skip validation during build-time commands (allows CARE images to build)
+        # Validation happens at runtime when settings are actually needed
+        if len(sys.argv) > 1 and sys.argv[1] in BUILD_TIME_COMMANDS:
+            return
+
+        from care_radiology.settings import plugin_settings
+
+        plugin_settings.validate()
